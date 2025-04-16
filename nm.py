be506df78ad6c2d949e0f5f32e12a2f450fd69fc
amirali <amir.moshi.ded@gmail.com>
2025-04-16 19:12:13 +0330

    brodcasting and slice

--- nm.py
+++ nm.py
@@ -17,3 +17,20 @@ print(c)
 b2 = np.array([5,6])
 con = np.concatenate((a,b2))
 print(con)
+
+# math things or brodcasting
+x = ([10])
+i = x * b2
+print(i)
+
+# slice
+a2 = a[1:3]
+print(a2)
+a3 = a[0:3:2]
+print(a3)
+
+b3 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+b4 = b3[b3>3]
+b5 = b3[b3<4]
+print(b4)
+print(b5)
