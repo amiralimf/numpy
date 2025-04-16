5ed8a99553425c025324d0b9b5ba07c9ee7856e4
amirali <amir.moshi.ded@gmail.com>
2025-04-16 19:43:28 +0330

    numpy part 2/tensor

--- nm2.py
+++ nm2.py
@@ -16,3 +16,32 @@ print(t)
 # you say you want 5 number between 0 and 10
 s = np.linspace(0, 10, num=5)
 print(s)
+
+
+# matrix 2 spaces 
+a = np.array([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8]
+])
+print("a -----------")
+print(a.ndim)
+print(a.size)
+print(a.shape)
+
+# tensor spaec more than 2 
+b = np.array([
+    [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+    ],
+    [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+    ]
+])
+print("b -----------")
+print(b.ndim)
+print(b.shape)
+print(b.size)
